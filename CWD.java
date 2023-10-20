ac0be37076cb7e1f35a898d32c0cf54f16bef747
Sayantan Karan <112612792+Sayantan73@users.noreply.github.com>
2023-10-20 23:58:19 +0530

    Create CWD.java

---
+++ CWD.java
@@ -0,0 +1,16 @@
+// Java Program to Find 
+// current working directory 
+import java.io.*; 
+
+public class CWD { 
+	
+	public static void main(String[] args) 
+	{ 
+
+		// Getting the path of system property 
+		String path = System.getProperty("user.dir"); 
+
+		// Printing the path of the working Directory 
+		System.out.println("Working Directory = " + path); 
+	} 
+}
